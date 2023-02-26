091caa39035b2a23065f5fde43d9c6b98178b229
gemcalc <126359376+gemcalc@users.noreply.github.com>
2023-02-26 21:34:16 +0000

    w

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function calculate() {
   let word = document.getElementById("word").value.toUpperCase();
-  let result = document.getElementById("result").getElementsByTagName("span")[0];
-  let sum = 0;
+  let eoResult = document.getElementById("english-ordinal").getElementsByTagName("span")[0];
+  let rdResult = document.getElementById("reduced").getElementsByTagName("span")[0];
+  let eoSum = 0;
+  let rdSum = 0;
 
   for (let i = 0; i < word.length; i++) {
     let letter = word.charCodeAt(i) - 64;
-    sum += letter;
+    eoSum += letter;
+    rdSum += (letter % 9) || 9;
   }
 
-  result.innerText = sum;
+  eoResult.innerText = eoSum;
+  rdResult.innerText = rdSum;
 }
