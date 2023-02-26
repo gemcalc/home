b58b9b9235f19b9af3c1f7da6973e988d310f653
gemcalc <126359376+gemcalc@users.noreply.github.com>
2023-02-26 21:49:52 +0000

    java

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-function calculate() {
-  const word = document.getElementById("word").value.toUpperCase();
+// Get HTML elements
+const textInput = document.getElementById("text-input");
+const englishOrdinalOutput = document.getElementById("english-ordinal-output");
+const reducedOutput = document.getElementById("reduced-output");
 
+// Add event listener to text input
+textInput.addEventListener("input", () => {
+  // Get input text and convert to uppercase
+  const inputText = textInput.value.toUpperCase();
+
+  // Calculate English Ordinal value
   let englishOrdinalValue = 0;
-  let reducedValue = 0;
-
-  for (let i = 0; i < word.length; i++) {
-    const letterValue = word.charCodeAt(i) - 64;
-
+  for (let i = 0; i < inputText.length; i++) {
+    const letterValue = inputText.charCodeAt(i) - 64;
     englishOrdinalValue += letterValue;
-    reducedValue += parseInt(letterValue.toString().slice(-1));
   }
+  englishOrdinalOutput.innerText = englishOrdinalValue || "0";
 
-  document.getElementById("english-ordinal").textContent = englishOrdinalValue;
-  document.getElementById("reduced").textContent = reducedValue;
-}
+  // Calculate Reduced value
+  let reducedValue = englishOrdinalValue;
+  while (reducedValue > 9) {
+    let tempValue = 0;
+    for (let i = 0; i < reducedValue.toString().length; i++) {
+      tempValue += parseInt(reducedValue.toString()[i]);
+    }
+    reducedValue = tempValue;
+  }
+  reducedOutput.innerText = reducedValue || "0";
+});
