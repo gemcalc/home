4326d2405f56257301d430fc3b864bcc3f95f69c
gemcalc <126359376+gemcalc@users.noreply.github.com>
2023-02-26 21:31:32 +0000

    remove

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 function calculate() {
-    let word = document.getElementById("word").value.toUpperCase();
-    let cipher = parseInt(document.getElementById("cipherSelect").value);
-    let result1 = document.getElementById("result1").getElementsByTagName("span")[0];
-    let result2 = document.getElementById("result2").getElementsByTagName("span")[0];
-    let sum1 = 0;
-    let sum2 = 0;
-  
-    for (let i = 0; i < word.length; i++) {
-      let letter = word.charCodeAt(i) - 64;
-      sum1 += letter;
-      sum2 += (letter % 9) || 9;
-    }
-  
-    result1.innerText = sum1;
-    result2.innerText = sum2;
+  let word = document.getElementById("word").value.toUpperCase();
+  let result = document.getElementById("result").getElementsByTagName("span")[0];
+  let sum = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    let letter = word.charCodeAt(i) - 64;
+    sum += letter;
   }
-  
+
+  result.innerText = sum;
+}
